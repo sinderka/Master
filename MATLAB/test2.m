c7ca1fa311582207eb8c4fb7b23d478a2f9bd006
Sindre Eskeland <sindre_e@hotmail.com>
2015-10-01 13:40:54 +0200

    Wavesolver(test2.m) fungerer noe i 2D, med rare kant-betingelser

--- MATLAB/test2.m
+++ MATLAB/test2.m
@@ -1,16 +1,16 @@
 %function [ utdata ] = wavesolver( m,n,k,prob,solmeth,conv,para )
-% Få til en naiv implementasjon av 2D waveequation
+% Få til en naiv implementasjon av 2D wave equation
 clear
 close all
-m = 5;
-k = 500;
+m = 111;
+k = 200;
 %%% Initsiell data
 utdata = zeros(1,3);
 X = linspace(0,1,m);
 hs =X(2)-X(1);
-T = linspace(0,30,k); 
+T = linspace(0,1,k);
 ht = T(2)-T(1);
-A = -1/hs^2*gallery('poisson', m);
+A = -1/hs^2*gallery('poisson', m-2);
 
 ant = 1;
 
@@ -36,30 +36,33 @@ V = zeros(m^2,1);
 %V = (2-X.*(1-X))';
 %V = -sin(pi*X)';
 
-
-
-
+helpvector = zeros((m-2)^2,1);
+for i = 0:m-3
+    helpvector(i*(m-2)+1:i*(m-2)+m-2) = (i+1)*m+2:m-1 +(1+ i)*m;
+end
 
 %U(:,1) = 1;
 %for j = 0:m-1
-    U(:,2) = U(:,1)- ht * V(:) + 0.5*ht^2*(A*U(:,1) + F(:,1));
+
 %end
-if 0
-for i = 3:k
-    %for j = 0:m-1
-        U(:,i) = 2*U(:,i-1) - U(:,i-2) + ht^2*(A*U(:,i-1) + F(:,i-1));
-    %end
-    mesh(reshape(U(:,i),m,m))
-    axis([0,m,0,m,-1,1])
-    drawnow
-    pause(0.06)
-end
+if 1
+    U(helpvector,2) = U(helpvector,1) - ht * V(helpvector) + 0.5*ht^2*(A*U(helpvector,1) + F(helpvector,1));
+    for i = 3:k
+        %for j = 0:m-1
+        U(helpvector,i) = 2*U(helpvector,i-1) - U(helpvector,i-2) + ht^2*(A*U(helpvector,i-1) + F(helpvector,i-1));
+        %end
+        mesh(reshape(U(:,i),m,m))
+        axis([0,m,0,m,-1,1])
+        %set ( handles.axes2, 'Color', [1 0 1] )
+        drawnow
+        pause(0.05)
+    end
 end
 %mesh(U)
 
 %correctsolution = sin(pi*X)'*cos(pi*T);
 %correctsolution = sin(pi*X)'*cos(pi*T);
-sol =@(t,x,y) sin(pi*x)*sin(pi*y)*cos(sqrt(2)*t);
+sol = @(t,x,y) sin(pi*x)*sin(pi*y)*cos(sqrt(2)*pi*t);
 correctsolution = zeros(m^2,k);
 for j = 1:k
     for i = 1:m
@@ -72,15 +75,24 @@ end
 %figure(2)
 %mesh(correctsolution)
 if 1
-for i = 3:k
-    mesh(reshape(correctsolution(:,i),m,m))
-    axis([0,m,0,m,-1,1])
-    drawnow
-    pause(0.06)
-end
+    for i = 3:k
+        mesh(reshape(correctsolution(:,i),m,m))
+        axis([0,m,0,m,-1,1])
+        %set ( handles.axes2, )
+        drawnow
+        pause(0.05)
+    end
 end
 %figure(3)
 %mesh(U-correctsolution)
 error = max(max(max(abs(U-correctsolution))))
 
-
+if 1
+    for i = 3:k
+        mesh(reshape(U(:,i),m,m)-reshape(correctsolution(:,i),m,m))
+        axis([0,m,0,m,min(min(U-correctsolution)),max(max(U-correctsolution))])
+        %set ( handles.axes2, )
+        drawnow
+        pause(0.05)
+    end
+end
